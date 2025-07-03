b534b37723c9b7fdd6ca1c7a95a399f2fa2e74bd
mjlail719 <mjlail719@gmail.com>
2025-07-03 11:59:57 -0400

    Added movement functionality to the player

--- main.py
+++ main.py
@@ -18,16 +18,28 @@ def main():
     clock = pygame.time.Clock() 
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
-        player.draw(screen)
+        
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+        
+        
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
+        updatable.update(dt)
         
 
 
