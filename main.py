01d48cee990aa41260f950a35b3170578a81636e
mjlail719 <mjlail719@gmail.com>
2025-07-02 20:27:51 -0400

    added functionality of drawing the player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -17,11 +18,14 @@ def main():
     clock = pygame.time.Clock() 
     dt = 0
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
         
